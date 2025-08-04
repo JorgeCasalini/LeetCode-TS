1c652815ee97e9326848bcce41c13be8e042972d
jorgeCasalini <joshcasalini@gmail.com>
2025-08-04 16:50:15 -0300

    feat: Adiciona desafios sobre logica de programação com TypeScript

---
+++ src/questions/clase-2.ts
@@ -0,0 +1,9 @@
+//La funcion recebe un parametro del tipo number y retorna el resultado de la
+//  multiplicacion por 2.
+
+function multiPor2(number: number) {
+  return number * 2;
+}
+
+const multiplicacion = multiPor2(74);
+console.log(multiplicacion);
---
+++ src/questions/clase-3.ts
@@ -0,0 +1,8 @@
+//Crie una  funcion que recibe dos palavras (Strings) y retorna su concatenacion.
+//De manera resumida, deve juntar las dos palabras.
+function concatenacion(palabra1: string, palabra2: string) {
+  return palabra1 + " " + palabra2;
+}
+
+const juntarPalabras = concatenacion("Facultadad", "Informatica");
+console.log(juntarPalabras);
