1f37af7af05fcf12b6740d6493f18bb6211fb37e
jorgeCasalini <joshcasalini@gmail.com>
2025-08-31 15:29:02 -0300

    add questions 4 and 5

---
+++ src/questions/clase-4.ts
@@ -0,0 +1,8 @@
+//Crie una funcion que recibe un número y retorne true si positivo.
+
+function positivo(number: number) {
+  if (number >= 0) return true;
+}
+
+const esPositivo = positivo(4);
+console.log(esPositivo);
---
+++ src/questions/clase-5.ts
@@ -0,0 +1,9 @@
+//Crie una funcion que recibe un número y retorna su antecesor. Por ejemplo:
+//si el número es  la funcion debe retornar 4.
+
+function antecesor(n1: number) {
+  return n1 - 1;
+}
+
+const numero = antecesor(10);
+console.log(numero);
